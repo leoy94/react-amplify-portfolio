159aa8f77ac46efb82da93945d38222a5e24affc
leoyo <leoyousif@gmail.com>
2020-04-28 17:09:58 -0600

    added structural changes to rendering the body.jsx object

--- src/components/Body/body.jsx
+++ src/components/Body/body.jsx
@@ -4,18 +4,24 @@ import "./body.css";
 class Body extends Component {
   state = {
     currenttab: this.props.currenttab,
+    rows: [],
+    columns: [{id:1, text:""},{id:2, text:""},{id:3, text:""}],
+    columnsperrow: 3
   };
 
-  //array for rows
-  //imbedded array for columns (or "items")
+  getwindowsize  = () => {
+    let width = window.outerWidth;
+    console.log(width);
+    return width; 
+  }
 
-  renderHeader = () => {
+  renderHeaderTitle = () => {
     let {currenttab} = this.state;
     if(this.state.currenttab!=this.props.currenttab){
       currenttab = this.props.currenttab; 
       this.setState({currenttab: currenttab});
 
-    }
+    } 
     if(currenttab=="Home"){
     return <h1>{currenttab}</h1>
     }
@@ -24,52 +30,59 @@ class Body extends Component {
     }
   }
 
-  render() {
-    return (
+  renderColumns = () => {
+    let columns = this.state.columns.map((column) => {
+        column.text = " Lorem ipsum dolor sit amet, consectetur adipiscing elit"+
+         "Maecenas sit amet pretium urna. Vivamus venenatis velit nec"+
+         "neque ultricies, eget elementum magna tristique. Quisque"+
+         "vehicula, risus eget aliquam placerat, purus leo tincidunt eros"+
+         "eget luctus quam orci in velit. Praesent scelerisque tortor sed"+
+         "accumsan convallis."
+      return (
+      <div className="column" id={column.id}>
+         <h2>Column {column.id}</h2>
+         <p>{column.text}</p>
+      </div>
+      );
+    });
+    return columns;
+  }
+
+  
+  renderRows = () => {
+    return(
       <div className="row">
         <div className="column middle">
           {/* <h2>Main Content</h2> */}
-          {this.renderHeader()}
+          
           <div className="row">
-            <div className="column">
-              <h2>Column</h2>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Maecenas sit amet pretium urna. Vivamus venenatis velit nec
-                neque ultricies, eget elementum magna tristique. Quisque
-                vehicula, risus eget aliquam placerat, purus leo tincidunt eros,
-                eget luctus quam orci in velit. Praesent scelerisque tortor sed
-                accumsan convallis.
-              </p>
-            </div>
-
-            <div className="column">
-              <h2>Column</h2>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Maecenas sit amet pretium urna. Vivamus venenatis velit nec
-                neque ultricies, eget elementum magna tristique. Quisque
-                vehicula, risus eget aliquam placerat, purus leo tincidunt eros,
-                eget luctus quam orci in velit. Praesent scelerisque tortor sed
-                accumsan convallis.
-              </p>
-            </div>
-
-            <div className="column">
-              <h2>Column</h2>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Maecenas sit amet pretium urna. Vivamus venenatis velit nec
-                neque ultricies, eget elementum magna tristique. Quisque
-                vehicula, risus eget aliquam placerat, purus leo tincidunt eros,
-                eget luctus quam orci in velit. Praesent scelerisque tortor sed
-                accumsan convallis.
-              </p>
-            </div>
+          {this.renderColumns()}
           </div>
           <h2>If Youre Reading This, I love you MOM!</h2>
         </div>
       </div>
+    );
+  }
+
+  renderBody = () => {
+    let body_header =  this.renderHeaderTitle(); 
+    let body_rows = this.renderRows();
+    
+    return (
+      <div>
+        <div>{body_header}</div>
+        <div>{body_rows}</div>
+      </div>
+    );
+
+  }
+
+//array for rows
+//imbedded array for columns (or "items")
+
+  render() {
+    return (
+      this.renderBody()
     );
   }
 }
