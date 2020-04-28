7e0123c2763ac13881a0067849fc45471640e82b
leoyo <leoyousif@gmail.com>
2020-04-28 15:41:27 -0600

    added dynamic Headers

--- src/components/Body/body.jsx
+++ src/components/Body/body.jsx
@@ -2,16 +2,34 @@ import React, { Component } from "react";
 import "./body.css";
 
 class Body extends Component {
-  state = {};
+  state = {
+    currenttab: this.props.currenttab,
+  };
 
   //array for rows
   //imbedded array for columns (or "items")
+
+  renderHeader = () => {
+    let {currenttab} = this.state;
+    if(this.state.currenttab!=this.props.currenttab){
+      currenttab = this.props.currenttab; 
+      this.setState({currenttab: currenttab});
+
+    }
+    if(currenttab=="Home"){
+    return <h1>{currenttab}</h1>
+    }
+    else{
+      return <h1>{currenttab}</h1>
+    }
+  }
 
   render() {
     return (
       <div className="row">
         <div className="column middle">
-          <h2>Main Content</h2>
+          {/* <h2>Main Content</h2> */}
+          {this.renderHeader()}
           <div className="row">
             <div className="column">
               <h2>Column</h2>
