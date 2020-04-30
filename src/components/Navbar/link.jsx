821301f21f55f28b8d5293e9eaee91cdb3d74398
leoyo <leoyousif@gmail.com>
2020-04-30 10:46:45 -0600

    reducted code using a stateless functional component

--- src/components/Navbar/link.jsx
+++ src/components/Navbar/link.jsx
@@ -1,61 +1,34 @@
 //Layth Yousif 4/28/2020
 //This jsx file defines the structure of an Individual Link
 
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 //The Link class defines a nav bar links and updates when they it has been clicked
+//this is a good canditate for creating a stateless functional component
 class Link extends Component {
-  state = {
-    id: this.props.id,
-    linkkey: this.props.linkkey,
-    label: this.props.label,
-    link: this.props.link,
-    className: this.props.className,
-    active: this.props.active,
-  };
+	//uses the state_id to call the handleCurrentTabClick method in ./links.jsx
+	//via the props. action object passed in from ./links.jsx
+	//when it envokes handleCurrentTabClick react will update the necessary state
+	//depending on which embedded attributes changed
+	handleClick = () => {
+		return this.props.action(this.props.link.id);
+	};
 
-  //uses the state_id to call the handleCurrentTabClick method in ./links.jsx
-  //via the props. action object passed in from ./links.jsx
-  //when it envokes handleCurrentTabClick react will update the necessary state 
-  //depending on which embedded attributes changed
-  handleClick = () => {
-    return this.props.action(this.state.id);
-  };
-
-  //sets the active state & class state(used for css styling)
-  //reset is dynamically rendered on change
-  resetState = () => {
-    if (this.props.active != this.state.active) {
-      let new_active = this.props.active;
-      let { active } = this.state;
-      active = new_active;
-
-      //another way to set the state
-      //console.log("ResetStateTriggered");
-      this.setState({ active: active });
-
-      //another way to set the state
-      let new_className = active == true ? "active" : "";
-      this.setState({ className: new_className });
-    }
-  };
-
-  render = () => {
-    this.resetState();
-    return (
-      <div
-        href={this.state.id}
-        key={this.state.label}
-        className={this.state.className}
-        id={this.state.id}
-        onClick={this.handleClick}
-      >
-        {this.props.children}
-        {this.state.label}
-
-      </div>
-    );
-  };
+	render = () => {
+		return (
+			<div
+				href={this.props.link.id}
+				key={this.props.link.label}
+				className={this.props.link.className}
+				id={this.props.link.id}
+				key={this.props.link.id}
+				onClick={this.handleClick}
+			>
+				{/* {this.props.children} */}
+				{this.props.link.label}
+			</div>
+		);
+	};
 }
 
 export default Link;
--- src/components/Navbar/links.jsx
+++ src/components/Navbar/links.jsx
@@ -1,113 +1,99 @@
 //Layth Yousif 4/28/2020
 //This jsx file defines the structure of a List of Links
 //The class defines nav bar links and updates them when they have been clicked
-import React, { Component } from "react";
-import "./topnav.css";
-import Link from "./link.jsx";
+import React, { Component } from 'react';
+import './topnav.css';
+import Link from './link.jsx';
 
 //this class defines nav bar links and updates them when they have been clicked
 class Links extends Link {
-  state = {
-    currenttab: "Home",
-    links: [
-      {
-        id: "Home",
-        linkkey: "1",
-        label: "Home",
-        link: "#home",
-        className: "active",
-        active: true,
-      },
-      {
-        id: "Personal",
-        linkkey: "2",
-        label: "Personal",
-        className: "",
-        link: "header-link",
-        active: false,
-      },
-      {
-        id: "Resume",
-        linkkey: "3",
-        label: "Resume",
-        className: "",
-        link: "header-link",
-        active: false,
-      },
-      {
-        id: "Portfolio",
-        linkkey: "4",
-        label: "Portfolio",
-        className: "",
-        link: "header-link",
-        active: false,
-      },
-    ],
-  };
+	state = {
+		currenttab: 'Home',
+		links: [
+			{
+				id: 'Home',
+				linkkey: '1',
+				label: 'Home',
+				link: '#home',
+				className: 'active',
+				active: true
+			},
+			{
+				id: 'Personal',
+				linkkey: '2',
+				label: 'Personal',
+				className: '',
+				link: 'header-link',
+				active: false
+			},
+			{
+				id: 'Resume',
+				linkkey: '3',
+				label: 'Resume',
+				className: '',
+				link: 'header-link',
+				active: false
+			},
+			{
+				id: 'Portfolio',
+				linkkey: '4',
+				label: 'Portfolio',
+				className: '',
+				link: 'header-link',
+				active: false
+			}
+		]
+	};
 
-  //called by handeCurrentTab
-  //resets each of the links classes based 
-  //on if they are active or inactive - active = false or true
-  //and sets the class name
-  //classes and states used for css styling and dynamic rendering
-  resetClass = (currenttab) => {
-    let links = (this.state.links = this.state.links.map((link) => {
-      link.active = currenttab == link.label ? true : false;
-      link.className = link.active == true ? "active" : "";
-      return link;
-    }));
-    return links;
-  };
+	//called by handeCurrentTab
+	//resets each of the links classes based
+	//on if they are active or inactive - active = false or true
+	//and sets the class name
+	//classes and states used for css styling and dynamic rendering
+	resetClass = (currenttab) => {
+		let links = (this.state.links = this.state.links.map((link) => {
+			link.active = currenttab == link.label ? true : false;
+			link.className = link.active == true ? 'active' : '';
+			return link;
+		}));
+		return links;
+	};
 
-  //called by handeCurrentTab
-  //calls ascenting function embedded in the prop object
-  //this sets the current tab attribure for the topnav's state
-  handleCurrentTabChange = (currenttab) => {
-    this.props.action(currenttab);
-  };
+	//called by handeCurrentTab
+	//calls ascenting function embedded in the prop object
+	//this sets the current tab attribure for the topnav's state
+	handleCurrentTabChange = (currenttab) => {
+		this.props.action(currenttab);
+	};
 
+	//called by handleClick in ./link.jsx; passed into the Link Component as method via the render method
+	//used for on click of each nav bar item
+	//calls resetClass to set the active state &  class state(used for css styling)
+	//updates the state of each link
+	handleCurrentTab = (target) => {
+		let { currenttab } = this.state;
+		currenttab = target;
 
-  //called by handleClick in ./link.jsx; passed into the Link Component as method via the render method
-  //used for on click of each nav bar item
-  //calls resetClass to set the active state &  class state(used for css styling)
-  //updates the state of each link 
-  handleCurrentTab = (target) => {
+		//invokes resetClass to set the active class (used for css styling)
+		let newlinks = this.resetClass(currenttab);
 
-    let { currenttab } = this.state;
-    currenttab = target;
+		//updates the state of each link
+		this.setState({ currenttab: currenttab });
+		this.setState({ links: newlinks });
 
-    //invokes resetClass to set the active class (used for css styling)
-    let newlinks = this.resetClass(currenttab);
+		//invokes ascenting function
+		//calls ascenting function embedded in the prop object
+		//this sets the current tab attribure for the topnav's state
+		this.handleCurrentTabChange(currenttab);
+		// this.render(); //this line is unncessary
+	};
 
-    //updates the state of each link 
-    this.setState({ currenttab: currenttab });
-    this.setState({ links: newlinks });
-    
-    //invokes ascenting function
-    //calls ascenting function embedded in the prop object
-    //this sets the current tab attribure for the topnav's state
-    this.handleCurrentTabChange(currenttab);
-    // this.render(); //this line is unncessary
-  };
-
-  render = () => {
-    let linksmarkup = this.state.links.map((link) => {
-      return (
-        <Link
-          id={link.id}
-          label={link.label}
-          className={link.className}
-          link={link.link}
-          linkkey={link.linkkey}
-          href={link.link}
-          key={link.id}
-          active={link.active}
-          action={this.handleCurrentTab}
-        />
-      );
-    });
-    return linksmarkup;
-  };
+	render = () => {
+		let linksmarkup = this.state.links.map((link) => {
+			return <Link link={link} action={this.handleCurrentTab} />;
+		});
+		return linksmarkup;
+	};
 }
 
 export default Links;
